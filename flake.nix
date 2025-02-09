cdebbb86b161314baaee556a9c72dec8931be60d
Jakub <jakub@schwenkbeck.de>
2025-02-09 19:45:43 +0100

    configured flake.nix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,5 @@
-{
-  ## TODO: Change to your project's description:
-  description = "Nix template for Effekt projects";
+{ # Based on @jiribenes' effekt-nix template :)
+  description = "Lambda Calculus in Effekt";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
@@ -21,8 +20,7 @@
         effekt-lib = effekt-nix.lib.${system};
 
         ## Project configuration
-        # TODO: Change to your project's details:
-        pname = "effekt-template";         # package name
+        pname = "effekt-lambda";         # package name
         version = "0.1.0";                 # package version
         mainFile = "src/main.effekt";      # relative path to entrypoint (as a string)
         testFiles = [ "src/test.effekt" ]; # relative paths to tests (as a string)
